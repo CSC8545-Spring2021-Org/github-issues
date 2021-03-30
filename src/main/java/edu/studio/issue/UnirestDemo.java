8e067b80459f87a598dc54f04e1f86ca71f6bd4b
Utsav Phunyal <uphunyal@villanova.edu>
2021-03-30 16:37:43 -0700

    Implement printing all issues from github-issues API

--- src/main/java/edu/studio/issue/UnirestDemo.java
+++ src/main/java/edu/studio/issue/UnirestDemo.java
@@ -1,5 +1,23 @@
 package edu.studio.issue;
 
+import kong.unirest.Unirest;
+
 public class UnirestDemo {
+
+    protected static final String TEST_AREA_URL = "https://api.github.com/repos/CSC8545-Spring2021-Org/github-issues-8545-s21-uphunyal/issues?state=all";
+
+    public static void main(String[] args) {
+        UnirestDemo demo = new UnirestDemo();
+
+        demo.requestViaBearerToken(args);
+    }
+
+    protected void requestViaBearerToken(String[] args) {
+        String responseBody = Unirest.get(TEST_AREA_URL)
+                .header("Authorization", "Bearer " + args[0]).asString()
+                .getBody();
+
+        System.out.println(responseBody);
+    }
 
 }
