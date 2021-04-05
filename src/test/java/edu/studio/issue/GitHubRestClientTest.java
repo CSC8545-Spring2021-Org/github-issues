1318feffbcc886365f00495d7d3e65d4dffbff05
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-05 15:18:58 -0700

    Add test method for getting issues

--- src/test/java/edu/studio/issue/GitHubRestClientTest.java
+++ src/test/java/edu/studio/issue/GitHubRestClientTest.java
@@ -1,19 +1,21 @@
 package edu.studio.issue;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GitHubRestClientTest {
 
+    private GitHubRestClient client;
     @BeforeEach
     void setUp() throws Exception {
+        
+        client = new GitHubRestClient();
     }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void returnsIssuesFromGitHub() {
+        
+        
     }
 
 }
