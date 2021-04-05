a8407c0bdedc8b61b2c33b18f5e66cdf025ba7ed
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-05 15:33:24 -0700

    Add method to get issues

--- src/main/java/edu/studio/issue/GitHubRestClient.java
+++ src/main/java/edu/studio/issue/GitHubRestClient.java
@@ -2,4 +2,9 @@ package edu.studio.issue;
 
 public class GitHubRestClient {
 
+    public String getIssuesFromGitHub(String token) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
