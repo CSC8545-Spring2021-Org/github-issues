6a4a75309244525c01e210c20acdedcb8365145e
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-05 16:53:58 -0700

    Test method for not 200 OK response

--- src/test/java/edu/studio/issue/GitHubRestClientTest.java
+++ src/test/java/edu/studio/issue/GitHubRestClientTest.java
@@ -2,6 +2,7 @@ package edu.studio.issue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -14,16 +15,17 @@ import org.junit.jupiter.api.Test;
 class GitHubRestClientTest {
 
     private GitHubRestClient client;
+    private String token;
     @BeforeEach
     void setUp() throws Exception {
         
         client = new GitHubRestClient();
+        token = System.getProperty("bearer.token"); 
     }
 
     @Test
     void returnsIssuesFromGitHub() throws IOException {
         
-        String token = System.getProperty("bearer.token"); 
         assertNotNull(token);
         assertTrue(token.length() > 0);
         
@@ -34,6 +36,20 @@ class GitHubRestClientTest {
         
         String actualJson = client.getIssuesFromGitHub(token);
         assertEquals(expectedJson, actualJson);
+        
+    }
+    
+    @Test
+    void returnsNullIfNot200() {
+        
+        //Invalid token to setup 401 response
+        token="1230ahkk234jdfk";
+        assertNotNull(token);
+        assertTrue(token.length() > 0);
+        
+        String response = client.getIssuesFromGitHub(token);
+        
+        assertNull(response);
         
     }
 
