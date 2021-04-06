259fa938d685cade421df9df8d761c619ee137d9
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-06 11:14:16 -0700

    Add IssueTest class

---
+++ src/test/java/edu/studio/issue/IssueTest.java
@@ -0,0 +1,19 @@
+package edu.studio.issue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IssueTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
