f5efdff07be2561183cd83b5161b50c97a8f6a5a
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-06 11:52:38 -0700

    Add fields and their getter and setter

--- src/main/java/edu/studio/issue/User.java
+++ src/main/java/edu/studio/issue/User.java
@@ -2,4 +2,19 @@ package edu.studio.issue;
 
 public class User {
 
+    private long id;
+    private String login;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
