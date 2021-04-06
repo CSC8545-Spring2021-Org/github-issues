69e2c76e02de32c18e480cda2cbdf55d70e96341
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-06 11:27:09 -0700

    Write tests for properties

--- src/test/java/edu/studio/issue/IssueTest.java
+++ src/test/java/edu/studio/issue/IssueTest.java
@@ -1,19 +1,51 @@
 package edu.studio.issue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IssueTest {
+    
+    private Issue issue;
+    private User user;
 
     @BeforeEach
     void setUp() throws Exception {
+        issue = new Issue();
+        user = new User();
     }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void testProperties() {
+      
+        
+        issue.setId(42);
+        assertEquals(42, issue.getId());
+        
+        issue.setState("open");
+        assertEquals("open", issue.getState());
+        
+        issue.setTitle("Issue 1");
+        assertEquals("Issue 1", issue.getTitle());
+        
+        issue.setBody("This is the first issue.");
+        assertEquals("This is the first issue.", issue.getBody());
+        
+        Date expectedDate = new Date();
+        issue.setCreatedAt(expectedDate);
+        assertEquals(expectedDate, issue.getCreatedAt());
+        
+        issue.setClosedAt(expectedDate);
+        assertEquals(expectedDate, issue.getClosedAt());
+        
+        issue.setAssignee(user);
+        assertEquals(user, issue.getAssignee());
+        
+        issue.setUser(user);
+        assertEquals(user, issue.getUser());
     }
 
 }
