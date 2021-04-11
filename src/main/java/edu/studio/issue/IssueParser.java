8e06dcfd45df8f05f27d327b2f92853f61e02dcf
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-10 18:10:49 -0700

    Add auto generated parIssues method

--- src/main/java/edu/studio/issue/IssueParser.java
+++ src/main/java/edu/studio/issue/IssueParser.java
@@ -1,5 +1,12 @@
 package edu.studio.issue;
 
+import java.util.List;
+
 public class IssueParser {
+
+    public List<Issue> parseIssues(String json) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
