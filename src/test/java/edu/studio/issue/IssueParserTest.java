81393c9c5086bfe135fa570dd5f368759eac8988
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-10 18:09:42 -0700

    Test parsing of issues when valid json provided

--- src/test/java/edu/studio/issue/IssueParserTest.java
+++ src/test/java/edu/studio/issue/IssueParserTest.java
@@ -1,19 +1,35 @@
 package edu.studio.issue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IssueParserTest {
 
+    private IssueParser parser;
     @BeforeEach
     void setUp() throws Exception {
+        parser= new IssueParser();
     }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testParseIssueWithValidJson(){
+        IssueParser parser = new IssueParser();
+        String json = Files.readString(
+                Paths.get("src/test/resources/sample-output.txt")); 
+        
+        List<Issue> issues = parser.parseIssues(json);
+        assertNotNull(issues);
+        assertEquals(2, issues.size());  
+        
+        Issue issue0 = issues.get(0);
+        assertEquals(1, issue0.getId());
     }
 
 }
