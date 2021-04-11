2c505c8daef9ed53f95332dc0a3252c1c398b254
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-11 16:19:41 -0700

    Implement validatePat method

--- src/main/java/edu/studio/issue/IssueExporter.java
+++ src/main/java/edu/studio/issue/IssueExporter.java
@@ -2,4 +2,10 @@ package edu.studio.issue;
 
 public class IssueExporter {
 
+    protected boolean validatePat(String[] args) {
+        
+        return (!(args==null) && args.length==1 && !args[0].isEmpty() 
+                && !(args[0]==null)) ? true : false;
+    }
+
 }
