7c1fb1e19b54234f1d91ca248b9b34c8ffac56f3
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-11 15:58:50 -0700

    Add IssueExporter Test Class and test for valid PAT

---
+++ src/test/java/edu/studio/issue/IssueExporterTest.java
@@ -0,0 +1,19 @@
+package edu.studio.issue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IssueExporterTest {
+
+    private IssueExporter export;
+    @BeforeEach
+    void setUp() throws Exception {
+        export = new IssueExporter();
+    }
+
+    @Test
+    void testValidPat() {
+        
+    }
+
+}
