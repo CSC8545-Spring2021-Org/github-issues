3405641e7d2534753190eec439f72be5300ee9a3
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-11 17:32:27 -0700

    Add constant for error message

--- src/main/java/edu/studio/issue/IssueExporter.java
+++ src/main/java/edu/studio/issue/IssueExporter.java
@@ -2,6 +2,8 @@ package edu.studio.issue;
 
 public class IssueExporter {
 
+    public static final String ERROR_MESSAGE = "Make sure you are providing only one non empty and non null argument!";
+
     protected boolean validatePat(String[] args) {
 
         return (args == null || args[0] == null || args.length != 1 || args[0].isEmpty()) ? false : true;
