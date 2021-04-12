807c86a0d54c767c02c3f7e251d3649df530b2b1
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-11 18:09:56 -0700

    Implement main method, call for token validation, and instantiate
    necessary objects

--- src/main/java/edu/studio/issue/IssueExporter.java
+++ src/main/java/edu/studio/issue/IssueExporter.java
@@ -1,9 +1,26 @@
 package edu.studio.issue;
 
+import java.util.List;
+
 public class IssueExporter {
 
     protected static final String ERROR_MESSAGE = "Make sure you are providing only one non empty and non null argument!";
 
+    public static void main(String[] args) {
+        
+        IssueExporter exporter = new IssueExporter();
+        
+        if(!exporter.validatePat(args)) {
+           exporter.printErrorMessage();
+           exporter.exitSystem();
+        }
+        
+        GitHubRestClient client = new GitHubRestClient();
+        String jsonResponse = client.getIssuesFromGitHub(args[0]);
+        
+        IssueParser parser = new IssueParser();
+        List<Issue> issues = parser.parseIssues(jsonResponse);
+    }
     protected boolean validatePat(String[] args) {
 
         return (args == null || args[0] == null || args.length != 1 || args[0].isEmpty()) ? false : true;
