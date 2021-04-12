b7478b62b7f2e0174d1bb0bc6a0f8c262b5d7411
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-11 17:31:29 -0700

    Implement test for exiting the program for invalid PAT

--- src/test/java/edu/studio/issue/IssueExporterTest.java
+++ src/test/java/edu/studio/issue/IssueExporterTest.java
@@ -2,16 +2,26 @@ package edu.studio.issue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.ginsberg.junit.exit.ExpectSystemExitWithStatus;
 
 class IssueExporterTest {
 
     private IssueExporter export;
+    private ByteArrayOutputStream byteStream;
+    private PrintStream outputNet;
+    private static String separator = System.lineSeparator();
 
     @BeforeEach
     void setUp() throws Exception {
         export = new IssueExporter();
+        byteStream = new ByteArrayOutputStream();
+        outputNet = new PrintStream(byteStream);
     }
 
     @Test
@@ -46,5 +56,16 @@ class IssueExporterTest {
         assertEquals(false, actual);
 
     }
+    
+    @Test
+    @ExpectSystemExitWithStatus(0)
+    void testSystemExitOnInvalidPat() {
+        System.setOut(outputNet);
+        export.exitSystem();
+        String expectedOutput = IssueExporter.ERROR_MESSAGE + separator;
+        assertEquals(expectedOutput, byteStream.toString());
+        
+    }
+    
 
 }
